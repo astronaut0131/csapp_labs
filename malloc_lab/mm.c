3eff767d53467704db67c85914109e5f43da5b62
astronaut0131 <jzh19970131@gmail.com>
2019-02-03 17:53:31 +0800

    use better strategy

--- malloc_lab/mm.c
+++ malloc_lab/mm.c
@@ -14,6 +14,7 @@
 #include <assert.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "mm.h"
 #include "memlib.h"
@@ -61,7 +62,7 @@ team_t team = {
 /* get the payload pointer of previous block */
 #define PREV_BLK(bp) ((bp) - (GET(bp - 2 * SIZE_T_SIZE) & (~0x7)))
 /* the minimium size to call sbrk with */
-#define CHUNK_SIZE (ALIGN((1 << 12)))
+#define CHUNK_SIZE (ALIGN(mem_pagesize()))
 /* payload pointer of the prelogue block */
 #define PRELOGUE_BLK (mem_heap_lo() + SIZE_T_SIZE)
 /* get start address of the heap */
@@ -86,14 +87,15 @@ team_t team = {
 static void *linked_list_head = NULL;
 static void print_heap(char  *);
 static void insert_to_linked_list_head(void *);
-static void extend_block(void *ptr,size_t request_size, size_t total_size);
 static void remove_from_linked_list(void *);
 static void* linked_list_first_fit(size_t);
-static void place(void *, size_t, size_t);
+static void* place(void *, size_t, size_t, bool);
 static void *first_fit(size_t);
 static void *best_fit(size_t);
 void coalesce(void *);
 static void mm_memcpy(void *, void *, size_t);
+static int cnt = 0;
+static size_t min_malloc_size = 5 * SIZE_T_SIZE;
 /*
  *  print_heap - A debug helper function that traverse the heap and print information of all heaps
  *  msg is the addtional information to figure out where this function is called
@@ -104,8 +106,8 @@ static void print_heap(char *msg) {
 		printf("%s\n", msg);
 	printf("Heap status -------------------------------\n");
 	for (void *ptr = PRELOGUE_BLK; GET_SIZE(ptr) != 0; ptr = NEXT_BLK(ptr)) {
-		printf("payload start = %lx payload end = %lx size = %ld alloc = %ld header = %lx footer = %lx\n",
-				(long)ptr, (long)FTRP(ptr), GET_SIZE(ptr), GET_ALLOC(ptr), (long)HDRP(ptr), (long)FTRP(ptr));
+		printf("payload start = %lx payload end = %lx alloc = %ld size_header %d size_footer %d\n",
+				(long)ptr, (long)FTRP(ptr), GET_ALLOC(ptr), (*(long *)HDRP(ptr)) & (~0x7), (*(long*)FTRP(ptr)) & (~0x7));
 	}
 }
 
@@ -223,39 +225,34 @@ static void* linked_list_first_fit(size_t size) {
  * Explicitly using blk_size as a parameter instead of GET_SIZE(ptr) is for
  * cases in mm_realloc
  */
-static void place(void *ptr, size_t size, size_t blk_size) {
-	/* place assume ptr points to a free block */
-	remove_from_linked_list(ptr);
-	if (blk_size >= size + SIZE_T_SIZE * 5) {
-		PUT(HDRP(ptr),PACK(size,1));
-		PUT(FTRP(ptr),PACK(size,1));
-		insert_to_linked_list_head(NEXT_BLK(ptr));
-		PUT(HDRP(NEXT_BLK(ptr)),PACK(blk_size - size,0));
-		PUT(FTRP(NEXT_BLK(ptr)),PACK(blk_size - size,0));
+static void* place(void *ptr, size_t size, size_t blk_size, bool is_ptr_free) {
+	if (is_ptr_free)
+		remove_from_linked_list(ptr);
+	if (blk_size >= size + min_malloc_size ) { 
+		if (cnt % 2 == 0) {
+			PUT(HDRP(ptr),PACK(size,1));
+			PUT(FTRP(ptr),PACK(size,1));
+			PUT(HDRP(NEXT_BLK(ptr)),PACK(blk_size - size,0));
+			PUT(FTRP(NEXT_BLK(ptr)),PACK(blk_size - size,0));
+			insert_to_linked_list_head(NEXT_BLK(ptr));
+			cnt++;
+			return ptr;
+		} else {	
+			PUT(HDRP(ptr),PACK(blk_size - size,0));
+			PUT(FTRP(ptr),PACK(blk_size - size,0));
+			PUT(HDRP(NEXT_BLK(ptr)),PACK(size,1));
+			PUT(FTRP(NEXT_BLK(ptr)),PACK(size,1));
+			insert_to_linked_list_head(ptr);
+			cnt++;
+			return NEXT_BLK(ptr);
+		}
 	} else {
 		PUT(HDRP(ptr),PACK(blk_size,1));
 		PUT(FTRP(ptr),PACK(blk_size,1));
+		return ptr;
 	}
 }
 
-/*
- * extend_block - this function is similar to place, but ptr points to a block that is allocated or uninitialized
- * this function is useful in realloc
- */
-
-static void extend_block(void *ptr,size_t request_size, size_t total_size) {
-	/* extend block assumes the block is not a free block (allocated or uninitialized one) */
-	if (total_size >= request_size + SIZE_T_SIZE * 5) {
-		PUT(HDRP(ptr),PACK(request_size,1));
-		PUT(FTRP(ptr),PACK(request_size,1));
-		insert_to_linked_list_head(NEXT_BLK(ptr));	
-		PUT(HDRP(NEXT_BLK(ptr)),PACK(total_size - request_size,0));
-		PUT(FTRP(NEXT_BLK(ptr)),PACK(total_size - request_size,0));
-	} else {
-		PUT(HDRP(ptr),PACK(total_size,1));
-		PUT(FTRP(ptr),PACK(total_size,1));
-	}
-}
 
 /*
  * first_fit -Find the first block that satisfy with the request of size, return NULL if not found
@@ -295,11 +292,12 @@ void *mm_malloc(size_t size)
 {
 	/* add size with header and footer size, and align it */
 	size_t new_size = ALIGN(size + 2 * SIZE_T_SIZE);
+	min_malloc_size = min_malloc_size < new_size ? min_malloc_size : new_size;
+	min_malloc_size = min_malloc_size < SIZE_T_SIZE * 5 ? SIZE_T_SIZE * 5 : min_malloc_size;
 	void *pos = linked_list_best_fit(new_size);
 	/* a suitable free block found */
 	if (pos != NULL) {
-		place(pos,new_size,GET_SIZE(pos));
-		return pos;
+		return	place(pos,new_size,GET_SIZE(pos),true);
 	}
 	/* no free block found, ask for more memory */
 	size_t size_to_alloc = new_size > CHUNK_SIZE ? new_size : CHUNK_SIZE;
@@ -309,10 +307,9 @@ void *mm_malloc(size_t size)
 		/* new_addr doesn't point to a initialized free block */
 		/* so we shouldn't use place */
 		/* place assume new_addr in the free block linked list */
-		extend_block(new_addr, new_size, size_to_alloc);
 		/* move the epilogue to the end of the heap */
 		PUT(new_addr + size_to_alloc - SIZE_T_SIZE,PACK(0,1));
-		return new_addr;
+		return place(new_addr, new_size, size_to_alloc,false);
 	}
 }
 
@@ -399,12 +396,17 @@ void *mm_realloc(void *ptr, size_t size)
 		return ptr;
 	}
 	size_t new_size = ALIGN(size + 2 * SIZE_T_SIZE);
+	if (new_size == GET_SIZE(ptr)) return ptr;
+	if (new_size < GET_SIZE(ptr)) {
+		return place(ptr, new_size, GET_SIZE(ptr), false);
+	}
 	/* the next block is free and coalesced size is large enough for request size */
 	if (!GET_ALLOC(NEXT_BLK(ptr)) && (GET_SIZE(ptr) + GET_SIZE(NEXT_BLK(ptr))) >= new_size ) {
 		/* just coalesce two blocks */
+		size_t total_size = GET_SIZE(ptr) + GET_SIZE(NEXT_BLK(ptr));
 		remove_from_linked_list(NEXT_BLK(ptr));
-		extend_block(ptr, new_size, GET_SIZE(ptr) + GET_SIZE(NEXT_BLK(ptr)));
-		return ptr;
+		cnt = cnt % 2 == 0 ? cnt : cnt + 1;
+		return place(ptr, new_size, total_size, false);
 	/* the previous block is free ... */
 	} else if (!GET_ALLOC(PREV_BLK(ptr)) && (GET_SIZE(ptr) + GET_SIZE(PREV_BLK(ptr))) >= new_size) {
 		/* call to PREV_BLK will get wrong result after mm_memcpy, so make a copy of it first */
@@ -414,10 +416,13 @@ void *mm_realloc(void *ptr, size_t size)
 		/* mm_memcpy will cover the data in the free block */
 		remove_from_linked_list(PREV_BLK(ptr));
 		/* start of the block now lies in previous block, so we have to copy data */
-		mm_memcpy(prev_ptr, ptr, GET_SIZE(ptr) - 2 * SIZE_T_SIZE);		
+		if (total_size >= new_size + min_malloc_size && cnt % 2 != 0) {
+			mm_memcpy(prev_ptr + total_size - new_size, ptr, GET_SIZE(ptr) - 2 * SIZE_T_SIZE);
+		} else {
+			mm_memcpy(prev_ptr, ptr, GET_SIZE(ptr) - 2 * SIZE_T_SIZE);		
+		}
 		/* coalesce blocks */
-		extend_block(prev_ptr, new_size, total_size);
-		return prev_ptr;
+		return place(prev_ptr, new_size, total_size,false);
 	/* similar */
 	} else if (!GET_ALLOC(PREV_BLK(ptr)) && !GET_ALLOC(NEXT_BLK(ptr)) && (GET_SIZE(ptr) + GET_SIZE(PREV_BLK(ptr)) + GET_SIZE(NEXT_BLK(ptr)) >= new_size)) {
 		void* prev_ptr = PREV_BLK(ptr);
@@ -425,9 +430,12 @@ void *mm_realloc(void *ptr, size_t size)
 		size_t total_size = GET_SIZE(ptr) + GET_SIZE(prev_ptr) + GET_SIZE(next_ptr);
 		remove_from_linked_list(PREV_BLK(ptr));
 		remove_from_linked_list(NEXT_BLK(ptr));
-		mm_memcpy(prev_ptr, ptr, GET_SIZE(ptr) - 2 * SIZE_T_SIZE);
-		extend_block(prev_ptr, new_size, total_size);
-		return prev_ptr;
+		if (total_size >= new_size + min_malloc_size && cnt % 2 != 0) {
+			mm_memcpy(prev_ptr + total_size - new_size, ptr, GET_SIZE(ptr) - 2 * SIZE_T_SIZE);
+		} else {
+			mm_memcpy(prev_ptr, ptr, GET_SIZE(ptr) - 2 * SIZE_T_SIZE);
+		}
+		return place(prev_ptr, new_size, total_size, false);
 	}
 	/*no coalescing is possible, asking for more memory */
 	void* new_ptr = mm_malloc(size);
