0224cfd523434315742c582a5ff81c22138c55a3
astronaut0131 <519537870@qq.com>
2019-02-03 18:31:54 +0800

    use segregated list

--- malloc_lab/mm.c
+++ malloc_lab/mm.c
@@ -15,10 +15,9 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdbool.h>
-
 #include "mm.h"
 #include "memlib.h"
-
+#include <math.h>
 /*********************************************************
  * NOTE TO STUDENTS: Before you do anything else, please
  * provide your team information in the following struct.
@@ -62,13 +61,14 @@ team_t team = {
 /* get the payload pointer of previous block */
 #define PREV_BLK(bp) ((bp) - (GET(bp - 2 * SIZE_T_SIZE) & (~0x7)))
 /* the minimium size to call sbrk with */
-#define CHUNK_SIZE (ALIGN(mem_pagesize()))
+#define CHUNK_SIZE (ALIGN(mem_pagesize() + 2 * SIZE_T_SIZE))
 /* payload pointer of the prelogue block */
-#define PRELOGUE_BLK (mem_heap_lo() + SIZE_T_SIZE)
+#define PRELOGUE_BLK (mem_heap_lo() + segregated_list_num * 8 + SIZE_T_SIZE)
 /* get start address of the heap */
 #define HEAP_START (mem_heap_lo())
 /* get end address of the heap */
 #define HEAP_END (mem_heap_hi() + 1)
+#define LIST_HEAD_SIZE (segregated_list_num * 8)
 /* get the payload pointer of previous free block */
 #define PREV_FREE_BLK(bp) ((void *)(*(long *)(bp)))
 /* get the payload pointer of next free block */
@@ -83,18 +83,18 @@ team_t team = {
 /*    +--------+----------------+-----------------+--------------+--------+    */
 /*    | 8      | 8              | 8               | n            | 8      |    */
 /*    +--------+----------------+-----------------+--------------+--------+    */
-/* head of the explicit free block linked list */
-static void *linked_list_head = NULL;
+/* using n segregate list */
+static int segregated_list_num = 18;
+/* allocate n * 8 bytes = 8n bytes at the begining of the heap for them */
+static void **segregated_list;
+static int cnt = 0;
 static void print_heap(char  *);
 static void insert_to_linked_list_head(void *);
 static void remove_from_linked_list(void *);
 static void* linked_list_first_fit(size_t);
 static void* place(void *, size_t, size_t, bool);
-static void *first_fit(size_t);
-static void *best_fit(size_t);
 void coalesce(void *);
 static void mm_memcpy(void *, void *, size_t);
-static int cnt = 0;
 static size_t min_malloc_size = 5 * SIZE_T_SIZE;
 /*
  *  print_heap - A debug helper function that traverse the heap and print information of all heaps
@@ -106,75 +106,105 @@ static void print_heap(char *msg) {
 		printf("%s\n", msg);
 	printf("Heap status -------------------------------\n");
 	for (void *ptr = PRELOGUE_BLK; GET_SIZE(ptr) != 0; ptr = NEXT_BLK(ptr)) {
-		printf("payload start = %lx payload end = %lx alloc = %ld size_header %d size_footer %d\n",
-				(long)ptr, (long)FTRP(ptr), GET_ALLOC(ptr), (*(long *)HDRP(ptr)) & (~0x7), (*(long*)FTRP(ptr)) & (~0x7));
+		printf("payload start = %lx payload end = %lx size = %ld alloc = %ld header = %lx footer = %lx\n",
+				(long)ptr, (long)FTRP(ptr), GET_SIZE(ptr), GET_ALLOC(ptr), (long)HDRP(ptr), (long)FTRP(ptr));
 	}
 }
 
-/* 
- * print_linked_list - A debug helper function that traverse the free block linked list and print information
- */
-static void print_linked_list(char *msg) {
+static void print_segregated_list(char *msg) {
 	if (msg[0] != '\0')
 		printf("%s\n", msg);
-	printf("Linked list status -------------------------------\n");
-	for (void* ptr = linked_list_head; ptr != NULL; ptr = NEXT_FREE_BLK(ptr)) {
-		printf("prev: %lx current: %lx next: %lx\n", (long)PREV_FREE_BLK(ptr), (long)ptr, (long)NEXT_FREE_BLK(ptr));
+	printf("Segregated list status ----------------------\n");
+	for (int i = 0; i < segregated_list_num; i++) {
+		printf("List %d:",i);
+		if (segregated_list[i] != NULL) {
+			for (void *p = segregated_list[i]; p != NULL; p = NEXT_FREE_BLK(p)) {
+				printf("%lx(%ld) -> ", (long)p, GET_SIZE(p));
+			}
+		}
+		printf("NULL\n");
 	}
+}
+
+static void init_segregated_list(void *addr) {
+	segregated_list = addr;
+	for (int i = 0; i < segregated_list_num; i++) {
+		segregated_list[i] = NULL;
+	}
+}
+
+static int dispatch_size(size_t size) {
+	int x = log(size) / log(2);
+	x = x > 6 ? x - 6 : 0;
+	x = x < segregated_list_num ? x : segregated_list_num - 1;
+	return x;
 }
 
 /*
  * insert_to_linked_list_head - insert a free block pointer ptr to the head of the free block linked list
  */
 static void insert_to_linked_list_head(void *ptr) {
-	if (linked_list_head == NULL) {
+	#ifdef DEBUG
+	printf("insert %lx size %d\n",ptr,GET_SIZE(ptr));
+	#endif
+	size_t size = GET_SIZE(ptr);
+	int index = dispatch_size(size);
+	if (segregated_list[index] == NULL) {
 		SET_PREV_FREE_BLK(ptr,0);
 		SET_NEXT_FREE_BLK(ptr,0);
 	} else {
-		SET_PREV_FREE_BLK(linked_list_head, ptr);
-		SET_NEXT_FREE_BLK(ptr, linked_list_head);
+		SET_PREV_FREE_BLK(segregated_list[index], ptr);
+		SET_NEXT_FREE_BLK(ptr, segregated_list[index]);
 		SET_PREV_FREE_BLK(ptr, 0);
 	}
-	linked_list_head = ptr;
+	segregated_list[index] = ptr;
+	#ifdef DEBUG
+	print_segregated_list("after");
+	#endif
 }
 
 /*
  * remove_from_linked_list - remove a free block's pointer information from the free block linked list
  */
 static void remove_from_linked_list(void *ptr) {
+	#ifdef DEBUG
+	printf("remove %lx size %d\n",ptr,GET_SIZE(ptr));
+	#endif
+	size_t size = GET_SIZE(ptr);
+	int index = dispatch_size(size);
 	if (PREV_FREE_BLK(ptr) == NULL && NEXT_FREE_BLK(ptr) == NULL) {
-		linked_list_head = NULL;
+		segregated_list[index] = NULL;
 	} else if (PREV_FREE_BLK(ptr) != NULL && NEXT_FREE_BLK(ptr) == NULL) {
 		SET_NEXT_FREE_BLK(PREV_FREE_BLK(ptr), NULL);
 	} else if (PREV_FREE_BLK(ptr) == NULL && NEXT_FREE_BLK(ptr) != NULL) {
 		SET_PREV_FREE_BLK(NEXT_FREE_BLK(ptr), NULL);
-		linked_list_head = NEXT_FREE_BLK(ptr);
+		segregated_list[index] = NEXT_FREE_BLK(ptr);
 	} else {	
 		SET_NEXT_FREE_BLK(PREV_FREE_BLK(ptr), NEXT_FREE_BLK(ptr));
 		SET_PREV_FREE_BLK(NEXT_FREE_BLK(ptr), PREV_FREE_BLK(ptr));
 	}
+	#ifdef DEBUG
+	print_segregated_list("after");
+	#endif
 }
 
 /*
  * linked_list_best_fit - using the free block linked list to find the best free block for size_t size
  */
 static void* linked_list_best_fit(size_t size) {
-	if (linked_list_head == NULL) {
-		return NULL;
-	}
-	if (NEXT_FREE_BLK(linked_list_head) == NULL && PREV_FREE_BLK(linked_list_head) == NULL) {
-		if (GET_SIZE(linked_list_head) >= size) return linked_list_head;
-		else return NULL;
-	}
+	int index = dispatch_size(size);
 	size_t min_size = (1 << 30);
-	void *min_pos = NULL;
-	for (void *cursor = linked_list_head; cursor != NULL; cursor = NEXT_FREE_BLK(cursor)) {
-		if (GET_SIZE(cursor) >= size && GET_SIZE(cursor) < min_size) {
-			min_size = GET_SIZE(cursor);
-			min_pos = cursor;
+	void *min_pos = NULL;	
+	for (int i = index; i < segregated_list_num; i++) {
+		for (void *cursor = (void *)segregated_list[i]; cursor != NULL; cursor = NEXT_FREE_BLK(cursor)) {
+			if (GET_SIZE(cursor) >= size && GET_SIZE(cursor) < min_size) {
+				min_size = GET_SIZE(cursor);
+				min_pos = cursor;
+			}
 		}
+		if (min_pos) return min_pos;
 	}
-	return min_pos;
+	return NULL;
 }
 
 /* 
@@ -182,18 +212,20 @@ static void* linked_list_best_fit(size_t size) {
  */
 int mm_init(void)
 {
-	void *start_addr = mem_sbrk(CHUNK_SIZE);
+	size_t init_size = ALIGN(CHUNK_SIZE+LIST_HEAD_SIZE+3*SIZE_T_SIZE);
+	void *start_addr = mem_sbrk(init_size);
 	if (start_addr == (void *) -1) return -1;
+	init_segregated_list(start_addr);
+	start_addr += LIST_HEAD_SIZE;	
 	/* put the prelogue block */
 	PUT(start_addr, PACK(2*SIZE_T_SIZE,1));
 	PUT(start_addr + SIZE_T_SIZE, PACK(2*SIZE_T_SIZE,1));
 	/* put the epilogue block */
 	PUT(HEAP_END - SIZE_T_SIZE, PACK(0,1));
 	/* put the free block between prelogue and epilogue */
-	PUT(start_addr + SIZE_T_SIZE * 2, PACK(CHUNK_SIZE - 3 * SIZE_T_SIZE,0));
-	PUT(HEAP_END - SIZE_T_SIZE * 2, PACK(CHUNK_SIZE - 3 * SIZE_T_SIZE,0));
+	PUT(start_addr + SIZE_T_SIZE * 2, PACK(init_size - 3 * SIZE_T_SIZE - LIST_HEAD_SIZE,0));
+	PUT(HEAP_END - SIZE_T_SIZE * 2, PACK(init_size - 3 * SIZE_T_SIZE - LIST_HEAD_SIZE,0));
 	/* set linked list head to NULL cauz mm_init can be called more than once */
-	linked_list_head = NULL;
 	/* add linked list information to the block */
 	insert_to_linked_list_head(start_addr + SIZE_T_SIZE * 3);
 	return 0;
@@ -204,15 +236,11 @@ int mm_init(void)
  * the latest freed block is inserted to the head of the list
  */
 static void* linked_list_first_fit(size_t size) {
-	if (linked_list_head == NULL) {
-		return NULL;
-	}
-	if (NEXT_FREE_BLK(linked_list_head) == NULL && PREV_FREE_BLK(linked_list_head) == NULL) {
-		if (GET_SIZE(linked_list_head) >= size) return linked_list_head;
-		else return NULL;
-	}
-	for (void *cursor = linked_list_head; cursor != NULL; cursor = NEXT_FREE_BLK(cursor)) {
-		if (GET_SIZE(cursor) >= size) return cursor;
+	int index = dispatch_size(size);
+	for (int i = index; i < segregated_list_num; i++) {
+		for (void *cursor = segregated_list[i]; cursor != NULL; cursor = NEXT_FREE_BLK(cursor)) {
+			if (GET_SIZE(cursor) >= size) return cursor;
+		}
 	}
 	return NULL;
 }
@@ -253,37 +281,6 @@ static void* place(void *ptr, size_t size, size_t blk_size, bool is_ptr_free) {
 	}
 }
 
-
-/*
- * first_fit -Find the first block that satisfy with the request of size, return NULL if not found
- * this is used in implicit free list
- */
-
-static void *first_fit(size_t size) {
-	for (void* ptr = PRELOGUE_BLK; GET_SIZE(ptr) != 0; ptr = NEXT_BLK(ptr)) {
-		if (!GET_ALLOC(ptr) && GET_SIZE(ptr) >= size) {
-			return ptr;
-		}
-	}
-	return NULL;
-}
-
-/*
- * best_fit -Find the block with least free payload size that satisfy with the request of size, return NULL if not found
- * this is used in implicit free list
- */
-static void *best_fit(size_t size) {
-	size_t min_size = 1 << 30;
-	void *min_ptr = NULL;
-	for (void *ptr = PRELOGUE_BLK; GET_SIZE(ptr) != 0; ptr = NEXT_BLK(ptr)) {
-		if (!GET_ALLOC(ptr) && GET_SIZE(ptr) >= size && GET_SIZE(ptr) < min_size) {
-			min_size = GET_SIZE(ptr);
-			min_ptr = ptr;
-		}
-	}
-	return min_ptr;
-}
-
 /* 
  * mm_malloc - Allocate a block by incrementing the brk pointer.
  *     Always allocate a block whose size is a multiple of the alignment.
@@ -293,20 +290,17 @@ void *mm_malloc(size_t size)
 	/* add size with header and footer size, and align it */
 	size_t new_size = ALIGN(size + 2 * SIZE_T_SIZE);
 	min_malloc_size = min_malloc_size < new_size ? min_malloc_size : new_size;
-	min_malloc_size = min_malloc_size < SIZE_T_SIZE * 5 ? SIZE_T_SIZE * 5 : min_malloc_size;
-	void *pos = linked_list_best_fit(new_size);
+	min_malloc_size = min_malloc_size < 5 * SIZE_T_SIZE ? 5 * SIZE_T_SIZE : min_malloc_size;
+	void *pos = linked_list_first_fit(new_size);
 	/* a suitable free block found */
 	if (pos != NULL) {
-		return	place(pos,new_size,GET_SIZE(pos),true);
+		return place(pos,new_size,GET_SIZE(pos),true);
 	}
 	/* no free block found, ask for more memory */
-	size_t size_to_alloc = new_size > CHUNK_SIZE ? new_size : CHUNK_SIZE;
+	size_t size_to_alloc =  new_size > CHUNK_SIZE ? new_size : CHUNK_SIZE;
 	void *new_addr = mem_sbrk(size_to_alloc);
 	if (new_addr == (void *) -1) return NULL;
-	else {
-		/* new_addr doesn't point to a initialized free block */
-		/* so we shouldn't use place */
-		/* place assume new_addr in the free block linked list */
+	else {	
 		/* move the epilogue to the end of the heap */
 		PUT(new_addr + size_to_alloc - SIZE_T_SIZE,PACK(0,1));
 		return place(new_addr, new_size, size_to_alloc,false);
@@ -397,9 +391,7 @@ void *mm_realloc(void *ptr, size_t size)
 	}
 	size_t new_size = ALIGN(size + 2 * SIZE_T_SIZE);
 	if (new_size == GET_SIZE(ptr)) return ptr;
-	if (new_size < GET_SIZE(ptr)) {
-		return place(ptr, new_size, GET_SIZE(ptr), false);
-	}
+	if (new_size < GET_SIZE(ptr)) return place(ptr, new_size, GET_SIZE(ptr), false);
 	/* the next block is free and coalesced size is large enough for request size */
 	if (!GET_ALLOC(NEXT_BLK(ptr)) && (GET_SIZE(ptr) + GET_SIZE(NEXT_BLK(ptr))) >= new_size ) {
 		/* just coalesce two blocks */
@@ -416,13 +408,14 @@ void *mm_realloc(void *ptr, size_t size)
 		/* mm_memcpy will cover the data in the free block */
 		remove_from_linked_list(PREV_BLK(ptr));
 		/* start of the block now lies in previous block, so we have to copy data */
+		/* mm_memcpy must be perform before place, cauz place can overlap src data */
 		if (total_size >= new_size + min_malloc_size && cnt % 2 != 0) {
 			mm_memcpy(prev_ptr + total_size - new_size, ptr, GET_SIZE(ptr) - 2 * SIZE_T_SIZE);
 		} else {
 			mm_memcpy(prev_ptr, ptr, GET_SIZE(ptr) - 2 * SIZE_T_SIZE);		
 		}
 		/* coalesce blocks */
-		return place(prev_ptr, new_size, total_size,false);
+		return place(prev_ptr, new_size, total_size, false);
 	/* similar */
 	} else if (!GET_ALLOC(PREV_BLK(ptr)) && !GET_ALLOC(NEXT_BLK(ptr)) && (GET_SIZE(ptr) + GET_SIZE(PREV_BLK(ptr)) + GET_SIZE(NEXT_BLK(ptr)) >= new_size)) {
 		void* prev_ptr = PREV_BLK(ptr);
